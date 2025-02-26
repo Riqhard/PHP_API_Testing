8f4ff55aa8c91d9efef1fa132fd1a9b0fa02daed
Riku Kilpimaa <RikuKilpimaa@hotmail.fi>
2025-02-26 14:56:40 +0200

    form change to cancel for testing

--- epassiForm.php
+++ epassiForm.php
@@ -1,103 +1,67 @@
 <?php
 include 'interface.php';
-
-// Replace these with actual values
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $stamp = rand(1000, 65535);
-    $total = 0;
-
-    if (isset($_POST['amount'])){
-        $amount = $_POST['amount'];
-        $total += (double)$amount;
-
-        if (isset($_POST['fee'])){
-            $fee = $_POST['fee'];
-            $total += (double)$fee;
-        }else{
-            $fee = "";
-        }
-        if (isset($_POST['vat_value'])){
-            $vat_value = $_POST['vat_value'];
-        }else{
-            $vat_value = "";
-        }
-    }
-}else{
-    $stamp = rand(1000, 65535);
-    $amount = "0.00";
-    $fee = "0.00";
-    $vat_value = "6.0";
-    $total = 0;
-}
+include_once "payment_system/payments.php";
 
 
-// Replace these with actual values
+
 $secretKey = "1TRQVUMAUBX4";
 $returnUrl = "https://kilpimaari-htc3def2dpckc4ht.westeurope-01.azurewebsites.net/payment_confirm.php";
 $rejectUrl = "https://kilpimaari-htc3def2dpckc4ht.westeurope-01.azurewebsites.net/payment_confirm.php";
 $cancelUrl = "https://kilpimaari-htc3def2dpckc4ht.westeurope-01.azurewebsites.net/payment_confirm.php";
 
 
-$epassi = new EpassiGenerator("login", "key", $returnUrl, $cancelUrl, $rejectUrl, "https://prodstaging.Epassi.fi/e_payments/v2", True);
+$epassi = new EpassiVerifier("key", True);
+$paymentProcessor = new PaymentProcessor($mysqli, "", "", "Button Text", $returnUrl, $rejectUrl, $cancelUrl);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    [$ok, $paymentID, $error] = $paymentProcessor->processPayment("epassi", $_POST, "POST");
+    if ($error){
+        echo "<br>error: ";
+        echo $error;
+    }else{
+        echo "<br>paymentID: ";
+        echo $paymentID;
+    }
+
+}elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
+    [$ok, $paymentID, $error] = $paymentProcessor->processPayment;
+}
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
+
+
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Epassi Payment Tester</title>
-    <link rel="stylesheet" href="style.css">
-
+  <title>epassi response tester</title>
 </head>
+ 
 <body>
-    
-<div class="fullContainer">
-<h1>Epassi Payment Tester</h1>
+  <h1>epassi response tester</h1>
+ 
+  <?php
 
-    <div class="payment-container">
-        <h2>Payment Form</h2>
-        <form method="post" action="">
-                <label for="amount">Amount:</label><br>
-                <input type="number" id="amount" name="amount" step="0.01" required><br><br>
-                
-                <label for="fee">Fee (Optional):</label><br>
-                <input type="number" id="fee" name="fee" step="0.01"><br><br>
-                
-                <label for="vat_value">VAT Value (Sweden only, eg. 6.0) (Optional):</label><br>
-                <input type="number" id="vat_value" name="vat_value" step="0.1"><br><br>
-                
-                <input type="submit" value="Submit">
-        </form>
-    </div>
-
-    <div id="payment-details" class="payment-container">
-            <h2>Payment Details</h2>
-            <p>Amount: <?php echo $amount; ?> €</p>
-            <p>Fee: <?php echo $fee; ?> €</p>
-            <p>VAT Value: <?php echo $vat_value; ?> %</p>
-            <p>Total: <?php echo $total; ?> €</p>
-    </div> 
-
-
-
-    <div id="payment-container" class="payment-container">
-        <h2>Epassi Payment</h2>
-        <p>Please review the payment details below and click "Pay with Epassi" to proceed.</p>
-        <?php 
-        [$ok, $form] = $epassi->generateEpassiForm($stamp, $amount, $fee, $vat_value);
-        if ($ok) {
-            echo $form;
-        } else {
-            echo "<p id='error'><b><i>Invalid amount, fee or VAT value.</i></b></p>";
-        }
-        
-        ?>
-    </div>
-
-</div>
-
-
+  
+?>
+ 
+Form for testing the response handling:
+ 
+<form action="payment_confirm.php" method="post">
+  <input type="hidden" name="STAMP" value="123456">
+  <input type="hidden" name="PAID" value="1235.00">
+  <input type="hidden" name="MAC" value="1234567890">
+  <input type="submit" value="Test return">
+</form>
+ 
+<form action="payment_confirm.php" method="get">
+  <input type="hidden" name="error" value="error message">
+  <input type="submit" value="Test reject">
+</form>
+ 
+<form action="payment_confirm.php" method="get">
+  <input type="hidden" name="cancel" value="true">
+  <input type="submit" value="Test cancel">
+</form>
+ 
+ 
 </body>
 </html>
-
